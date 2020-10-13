7ba98455cfdd7724b409d29d4340d3328d669e67
Shoya-cyber <shoyasan0627@gmail.com>
2020-10-13 21:12:28 +0900

    userモデル単体テストコード記述

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname {Faker::Name.name}
+    email {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length: 6)
+    password {password}
+    password_confirmation {password}
+    family_name {'鈴木'}
+    first_name {'太郎'}
+    family_name_reading {'スズキ'}
+    first_name_reading {'タロウ'}
+    birth_date {'1930-1-1'}
+  end
+end
