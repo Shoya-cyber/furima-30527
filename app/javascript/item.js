5005bb4270a12b120335186ebdec59ba8a6c04a5
Shoya-cyber <shoyasan0627@gmail.com>
2020-10-14 20:40:50 +0900

    利益計算機能を追加

--- app/javascript/item.js
+++ app/javascript/item.js
@@ -1,4 +1,11 @@
 function item () {
-  console.log("非同期通信テスト");
-}
+  const itemPrice = document.getElementById("item-price");
+  const addTaxPrice = document.getElementById("add-tax-price");
+  const profit = document.getElementById("profit");
+  itemPrice.addEventListener('input', function(){
+    addTaxPrice.innerHTML = (itemPrice.value * 0.1);
+    profit.innerHTML = (itemPrice.value * 0.9);
+    
+  });
+};
 window.addEventListener('load', item);
