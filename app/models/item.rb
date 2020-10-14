57e1f06e41c1cbe1837c1150a8bad0be29ab4afe
Shoya-cyber <shoyasan0627@gmail.com>
2020-10-14 12:16:09 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,15 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  with_options presence: true do
+    validates :name
+    validates :info
+    validates :category_id
+    validates :sales_status_id
+    validates :shipping_fee_status_id
+    validates :prefecture_id
+    validates :scheduled_delivery_id
+    validates :price
+    validates :image
+  end
 end
