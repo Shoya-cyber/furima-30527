5f437bb76e12d3d470a03bea986c1a45c052fab8
Shoya-cyber <shoyasan0627@gmail.com>
2020-10-14 13:46:23 +0900

    バリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,14 +3,15 @@ class Item < ApplicationRecord
   has_one_attached :image
   
   with_options presence: true do
-    validates :name
-    validates :info
+    validates :name, length: { maximum: 40 }
+    validates :info, length: { maximum: 1000 }
     validates :category_id
     validates :sales_status_id
     validates :shipping_fee_status_id
     validates :prefecture_id
     validates :scheduled_delivery_id
-    validates :price
+    validates :price, format: {with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters."}
     validates :image
   end
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
 end
