066e70f3eb5dc706d8c6ed108a7785621b7d9eb3
Shoya-cyber <shoyasan0627@gmail.com>
2020-10-15 11:34:22 +0900

    itemモデル単体テストコード記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -20,7 +20,7 @@ class Item < ApplicationRecord
     validates :price, format: {with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters."}
     validates :image
   end
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range."}
 
   with_options numericality: { other_than: 1 } do
     validates :category_id
@@ -29,6 +29,6 @@ class Item < ApplicationRecord
     validates :prefecture_id
     validates :scheduled_delivery_id
   end
-
+  
 end
 
