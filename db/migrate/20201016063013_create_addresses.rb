02e765551447b3e0de20bf8280d42be293dd72b9
Shoya-cyber <shoyasan0627@gmail.com>
2020-10-16 15:51:05 +0900

    addressesテーブル作成

--- db/migrate/20201016063013_create_addresses.rb
+++ db/migrate/20201016063013_create_addresses.rb
@@ -1,7 +1,13 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
-
+      t.string     :postal_code,        null: false, default: ""
+      t.integer    :prefecture_id,      null: false
+      t.string     :city,               null: false, default: ""
+      t.string     :house_number,       null: false, default: ""
+      t.string     :building_name
+      t.string     :phone_number,       null: false, default: ""
+      t.references :order,              foreign_key: true
       t.timestamps
     end
   end
