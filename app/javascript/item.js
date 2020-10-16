5c1847816013995381de36fbe0ecc0865709bd74
Shoya-cyber <shoyasan0627@gmail.com>
2020-10-16 09:19:23 +0900

    利益計算の値を切り捨てた値になるように修正

--- app/javascript/item.js
+++ app/javascript/item.js
@@ -3,9 +3,8 @@ function item () {
   const addTaxPrice = document.getElementById("add-tax-price");
   const profit = document.getElementById("profit");
   itemPrice.addEventListener('input', function(){
-    addTaxPrice.innerHTML = (itemPrice.value * 0.1);
-    profit.innerHTML = (itemPrice.value * 0.9);
-    
+    addTaxPrice.innerHTML = Math.floor(itemPrice.value * 0.1);
+    profit.innerHTML = Math.floor(itemPrice.value * 0.9);
   });
 };
 window.addEventListener('load', item);
