86800b1bd9592df2e84598bb0d0315682fe16127
Shoya-cyber <shoyasan0627@gmail.com>
2020-10-18 01:12:13 +0900

    OrderAddressモデルにバリデーション記述

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,3 +1,17 @@
 class OrderAddress
   include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number
+
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+    validates :prefecture
+    validates :city
+    validates :house_number
+    validates :phone_number, format: {with: /\A\d{11}\z/, message: "is invalid. Not include hyphen(-)" }
+  end
+
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+
+
+
 end
