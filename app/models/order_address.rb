b8407c3b73febba532059c24dd49fb6de4037395
Shoya-cyber <shoyasan0627@gmail.com>
2020-10-18 01:24:47 +0900

    OrderAddressモデルにsaveメソッドを定義

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -12,6 +12,10 @@ class OrderAddress
 
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
+  def save
+    order = Order.create(user_id: user.id, order_id: order.id)
 
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
+  end
 
 end
