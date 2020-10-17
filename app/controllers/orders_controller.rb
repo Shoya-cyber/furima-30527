8181f04a6c2529fb8a0650a0a98cf87489fc45bc
Shoya-cyber <shoyasan0627@gmail.com>
2020-10-18 00:03:27 +0900

    ordersコントローラにトークンの情報をサーバーサイドで受け取れるように記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,40 @@
 class OrdersController < ApplicationController
 
   def index
+    @order = Order.new
   end
   
   def create
+    @order = Order.new(order_params)
+    Address.create(address_params(order))
+    if @order.valid?
+      pay_item
+      @order.save
+      redirect_to root_path
+    else
+      render 'index'
+    end
+
   end
 
   private
+
+  def order_params
+    params.require(:order).merge(token: params[:token], user_id: user.id, item_id: item.id)
+  end
+
+  def address_params(order)
+    params.permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(order_id: order.id)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @order.item.price
+        card: order_params[:token],
+        currency: 'jpy'
+      )
+    end
 
 end
 
