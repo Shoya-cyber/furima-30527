28516cab130a0c61f9c2a53c60e092f70bbe58a1
Shoya-cyber <shoyasan0627@gmail.com>
2020-10-18 18:19:47 +0900

    OrderAddressモデルの記述修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,11 +1,11 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :token, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number
+  attr_accessor :token, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id
 
   with_options presence: true do
     validates :token
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
-    validates :prefecture
+    validates :prefecture_id
     validates :city
     validates :house_number
     validates :phone_number, format: {with: /\A\d{11}\z/, message: "is invalid. Not include hyphen(-)" }
@@ -14,9 +14,9 @@ class OrderAddress
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
   def save
-    order = Order.create(token: token, user_id: user.id, order_id: order.id)
-
+    order = Order.create(token: token, user_id: user_id, item_id: item_id)
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
+
 
 end
